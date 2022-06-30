ce45d9e44190e03756d9095bedacd9501ff01cc1
Jack Meyers <mjmeyers@proton.me>
2022-06-30 12:29:37 -0500

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from '../features/campsite/campsitesSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { logger } from 'redux-logger';
 
 export const store = configureStore({
 	reducer: {
@@ -10,5 +11,7 @@ export const store = configureStore({
 		comments: commentsReducer,
 		partners: partnersReducer,
 		promotions: promotionsReducer
-	}
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat([ logger ])
 });
